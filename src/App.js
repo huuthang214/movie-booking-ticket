5de93cd741f52e6edd42e98e261a174548be955b
huuthang214 <huuthangqn99@gmail.com>
2022-12-02 22:20:32 +0700

    add project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import DemoGrid from './TailwindComponent/DemoGrid';
+
+function App() {
+  return (
+    <div className="App">
+      <DemoGrid />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TailwindComponent/DemoGrid.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function
+    () {
+    return (
+        <div className='container'>
+            <div className='grid grid-cols-3'>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+            </div>
+        </div>
+    )
+}
